0d7a169ac06b8f8a6d5474cd622be3a9f95e34de
Dmitry <dmitrychinyaev93@yandex.ru>
2023-06-20 23:51:01 +0300

    Admin 2.0

--- src/main/java/com/dmitrychinyaev/postsService/controller/UserController.java
+++ src/main/java/com/dmitrychinyaev/postsService/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.dmitrychinyaev.postsService.controller;
 
+import com.dmitrychinyaev.postsService.domain.Role;
+import com.dmitrychinyaev.postsService.domain.User;
 import com.dmitrychinyaev.postsService.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,5 +19,12 @@ public class UserController {
     public String userList(Model model){
         model.addAttribute("users",userService.findAllUsers());
         return "userList";
+    }
+
+    @GetMapping("{user}")
+    public String userEditForm(@PathVariable User user, Model model){
+        model.addAttribute("user", user);
+        model.addAttribute("roles", Role.values());
+        return "userEdit";
     }
 }
