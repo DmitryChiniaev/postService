40d90692765007834542cb5647d5bb9546c377e8
Dmitry <dmitrychinyaev93@yandex.ru>
2023-08-18 07:49:00 +0300

    Flyway migration

--- src/main/java/com/dmitrychinyaev/postsService/config/WebSecurityConfig.java
+++ src/main/java/com/dmitrychinyaev/postsService/config/WebSecurityConfig.java
@@ -22,7 +22,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/registration","/activate/*").permitAll()
+                .antMatchers("/", "/registration","/activate/*","/test").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
